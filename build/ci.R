21f3e2d6983924ae76087e59a1a98287be5a7941
Jarrett Meyer <meyer_jarrett@lilly.com>
2022-06-29 17:17:26 -0400

    lintr works

--- build/ci.R
+++ build/ci.R
@@ -1,7 +1,7 @@
 # ci.R
-# Continuous integraion scripts for R 
+# Continuous integraion scripts for R
 
 install.packages("lintr")
 install.packages("styler")
 
-lintr::lint("../src/")
+lintr::lint_dir("./src/")
